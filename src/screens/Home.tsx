867dabb11bb820ee805df30009f580c2836bd8c2
Vanderlei Furtuna <vander@pop-os.localdomain>
2023-01-23 21:08:25 -0300

    feat: add new page habit style

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { View, Text, ScrollView } from 'react-native';
 import { HabitDay, DAY_SIZE, HabitDayDisabled } from '../components/HabitDay';
 import { Header } from '../components/Header';
@@ -8,7 +9,10 @@ const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const datesFromYearStart = generateRangeDatesFromYearStart();
 const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
+
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -29,7 +33,10 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toString()} />
+            <HabitDay
+              key={date.toString()}
+              onPress={() => navigate('habit', { date: date.toISOString() })}
+            />
           ))}
           {amountOfDaysToFill > 0 &&
             Array.from({ length: amountOfDaysToFill }).map((_, index) => (
